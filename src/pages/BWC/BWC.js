1c36a2312fadfbc9572756ca061e3218e990457c
BanjoDave <wakkace@gmail.com>
2023-06-29 17:27:05 +0100

    bwc

--- src/pages/BWC/BWC.js
+++ src/pages/BWC/BWC.js
@@ -4,11 +4,53 @@ import "./BWC.css";
 
 const correctSequence = [216, 216, 72, 180];
 
+const emojiDescriptions = {
+  "0-0": "Hibiscus Slot Machine",
+  "0-1": "Money Face Poop",
+  "0-2": "Dazed Diamond",
+  "0-3": "VHS in Waste Basket",
+  "0-4": "UFO under Wave",
+  "0-5": "Earth-Sun",
+  "0-6": "Cool Thundercloud",
+  "0-7": "Shocked Banana",
+  "0-8": "Rudolf Bread",
+  "0-9": "Tiger Candy",
+  "1-0": "Fork and Knife with Infinity",
+  "1-1": "Brain Ghost",
+  "1-2": "Fuming",
+  "1-3": "Colourful Microbes",
+  "1-4": "Disguised Alien",
+  "1-5": "Moonlit Love Letter",
+  "1-6": "Monkey eating Satsuma",
+  "1-7": "Vomiting Cupcake",
+  "1-8": "Can of Saturn",
+  "1-9": "City Ablaze",
+  "2-0": "Diamond Pepper",
+  "2-1": "Mushroom Rabbits",
+  "2-2": "Shrinking Watermelon",
+  "2-3": "Goat eating Jack-o'-Lantern",
+  "2-4": "Newspaper Stump",
+  "2-5": "Visions of a Wilted Rose",
+  "2-6": "Shaking Full-Moon Face",
+  "2-7": "Arrow through Anatomical Heart",
+  "2-8": "Sad Meat on Bone",
+  "2-9": "Thoughtful Hotdog",
+  "3-0": "Drooling Star",
+  "3-1": "Shrugging Nazar",
+  "3-2": "Partying Cyclops",
+  "3-3": "Frozen Scorpion",
+  "3-4": "Monochrome Rainbow",
+  "3-5": "New Moon Face under a Box-and-Stick Trap",
+  "3-6": "Fossil Skull",
+  "3-7": "Robot Holding Rock Overhead",
+  "3-8": "Cactus Lips",
+  "3-9": "Maple Leaf Smoke",
+};
+
 const generateEmojiSlots = (selectedImages) =>
   selectedImages.map((emoji, index) => (
-    <div className="emoji-slot">
+    <div className="emoji-slot" key={index}>
       <img
-        key={index}
         src={emoji > -1 ? `/img/emojikitchen/${index}-${emoji}.png` : undefined}
         alt="emoji"
         className={`emoji-slot slot-${index} ${
@@ -30,13 +72,78 @@ const generateEmojiKeys = (selectedImages, handleClick) =>
         >
           <img
             src={`/img/emojikitchen/${listIndex}-${imageIndex}.png`}
-            alt="emoji"
+            alt={emojiDescriptions[`${listIndex}-${imageIndex}`]}
+            title={emojiDescriptions[`${listIndex}-${imageIndex}`]}
             onMouseDown={() => handleClick(listIndex, imageIndex)}
           />
         </div>
       ))}
     </div>
   ));
+
+const AccessScreen = ({ accessLabel, secretCode }) => (
+  <div className="monitor">
+    <div className="screen">
+      <div className="center">
+        <img
+          src="/bwc/Banjo-Warez-Corporation.png"
+          alt="Banjo Warez Corporation"
+          loading="lazy"
+          height="256px"
+        />
+      </div>
+      <div className="screen-page">
+        <div className="center">
+          <div className="sEcUrItY">
+            <br />
+            <u> {accessLabel} </u>
+            <br />
+            <br />
+          </div>
+        </div>
+        {secretCode && (
+          <p>
+            Congratulations on figuring out the password, bypassing security
+            protocol, or finding the GitHub!
+            <br />
+            Send secret code <u className="sEcUrItY"> {secretCode} </u> to
+            BanjoDave right now for a BWC Black Keycard granting free top-tier
+            Banjo content for life.
+          </p>
+        )}
+      </div>
+    </div>
+    <div className="inlay">BWC Special Access Device</div>
+  </div>
+);
+
+const MainScreen = ({ selectedImages, handleClick }) => (
+  <div className="monitor">
+    <div className="screen pin-screen">
+      <div className="emoji-pin-pad">
+        <div className="selected-emojis">
+          {generateEmojiSlots(selectedImages)}
+        </div>
+        <div>{generateEmojiKeys(selectedImages, handleClick)}</div>
+        <p className="hint">
+          ABOVEDANCETEMPESTSASILENTROARORCOOLTUNE
+          <br />
+          ANENCHANTEDJUNGLEBONGOCLOAKEDDAWNSBLUSH
+          <br />
+          CHANGINGWHISPERSOFASUMMERSSWEETCRESCENT
+          <br />
+          DARKEYESSOUGHTTOCAPTURETHEBEAUTYOFNIGHT
+        </p>
+        <p>
+          {/*<Link to="../thank-you" className="access-link">
+            Password on Patreon Free Tier
+          </Link>*/}
+        </p>
+      </div>
+    </div>
+    <div className="inlay">BWC Special Access Device</div>
+  </div>
+);
 
 function EmojiPassword() {
   const [selectedImages, setSelectedImages] = useState(Array(4).fill(-1));
@@ -80,86 +187,14 @@ function EmojiPassword() {
   return (
     <>
       {access === 2 ? (
-        <div className="monitor">
-          <div className="screen">
-            <div className="center">
-              <img
-                src="/bwc/Banjo-Warez-Corporation.png"
-                alt="Banjo Warez Corporation"
-                loading="lazy"
-                height="256px"
-              />
-            </div>
-            <div className="screen-page">
-              <div className="center">
-                <div className="sEcUrItY">
-                  <br />
-                  <u> TOP SECRET//SAR-MAPLE SMOKE/SAR-COOL TUNE//WAIVED </u>
-                  <br />
-                  <u> LEVEL 2 [TWO] ACCESS </u>
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="inlay">BWC Special Access Device</div>
-        </div>
+        <AccessScreen accessLabel="TOP SECRET//SAR-MAPLE SMOKE/SAR-COOL TUNE//WAIVED" />
       ) : access === 1 ? (
-        <div className="monitor">
-          <div className="screen">
-            <div className="center">
-              <img
-                src="/bwc/Banjo-Warez-Corporation.png"
-                alt="Banjo Warez Corporation"
-                loading="lazy"
-                height="256px"
-              />
-            </div>
-            <div className="screen-page">
-              <div className="center">
-                <div className="sEcUrItY">
-                  <br />
-                  <u> SECRET//SPECIAL ACCESS REQUIRED-COOL TUNE </u>
-                  <br />
-                  <u> LEVEL 1 [ONE] ACCESS </u>
-                </div>
-                <br />
-                Congratulations on figuring out the password, bypassing security
-                protocol, or finding the GitHub!
-                <br />
-                Send secret code PSYCHO-GORILLA to BanjoDave right now for a BWC
-                Black Key Card granting free top-tier Banjo content for life.
-              </div>
-            </div>
-          </div>
-          <div className="inlay">BWC Special Access Device</div>
-        </div>
+        <AccessScreen
+          accessLabel="SECRET//SPECIAL ACCESS REQUIRED-COOL TUNE"
+          secretCode="PSYCHO-GORILLA"
+        />
       ) : (
-        <div className="monitor">
-          <div className="screen pin-screen">
-            <div className="emoji-pin-pad">
-              <div className="selected-emojis">
-                {generateEmojiSlots(selectedImages)}
-              </div>
-              <div>{generateEmojiKeys(selectedImages, handleClick)}</div>
-              <p className="hint">
-                abovetempestsdancewovenintothecelestial
-                <br />
-                anenchantedjunglebongocloakeddawnsblush
-                <br />
-                changingwhispersofasummerssweetcrescent
-                <br />
-                darkeyessoughttocapturethebeautyofnight
-              </p>
-              <p>
-                <Link to="../thank-you" className="access-link">
-                  Password on Patreon Free Tier
-                </Link>
-              </p>
-            </div>
-          </div>
-          <div className="inlay">BWC Special Access Device</div>
-        </div>
+        <MainScreen selectedImages={selectedImages} handleClick={handleClick} />
       )}
     </>
   );
