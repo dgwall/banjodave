e1081b15ddad0163de1f607951afd225d599cb34
BanjoDave <wakkace@gmail.com>
2023-07-17 17:00:18 +0100

    Deck view auto switch fix and access set temporarily to 1

--- src/pages/Cards/Cards.js
+++ src/pages/Cards/Cards.js
@@ -14,7 +14,7 @@ import {
 
 const ITEMS_HOMEPAGE = 36;
 const ITEMS_PER_PAGE = 8;
-const ACCESS_LEVEL = 0;
+const ACCESS_LEVEL = 1;
 
 const getTheme = (themeName) => {
   return cardThemes.find((theme) => theme.name === themeName) || {};
@@ -106,7 +106,6 @@ function Cards() {
   // handler for card click
   const handleCardClick = (card) => {
     setSelectedCard(card);
-    setGroupMode("card");
     setViewMode("similar");
     setCurrentPage(1);
     setSearchTerm("");
